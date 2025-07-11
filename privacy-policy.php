2a0e6528a0c224cfa97f511564edfcceda50388c
gfaUnDead <contact@gfaundead.stream>
2025-07-11 18:04:37 +1000

    Fix indentation in privacy policy section
    Corrected the indentation of the Privacy Policy section in privacy-policy.php for improved code readability and consistency. No content changes were made.

--- privacy-policy.php
+++ privacy-policy.php
@@ -72,73 +72,65 @@
 				</div>
 			</div>
 		</section>
-
-						<!-- Privacy Policy Section -->
-						<section id="pp" class="section">
-							<div class="container">
-								<div class="has-text-centered mb-6">
-									<h2 class="title is-2">Privacy Policy</h2>
-								</div>
-								
-								<div class="content">
-									<div class="box">
-										<h3 class="title is-4">Who we are</h3>
-										<p>Our website addresses are:</p>
-										<ul>
-											<li>lochstudios.com</li>
-											<li>lochstudios.info</li>
-											<li>lochs.media</li>
-											<li>getyourwebsite.net.au</li>
-											<li>lscomputer.repair</li>
-											<li>sentral.network</li>
-											<li>mystorage-dashboard.com</li>
-											<li>yourhost.zone</li>
-											<li>yourminecraft.host</li>
-										</ul>
-									</div>
-
-									<div class="box">
-										<h3 class="title is-4">What personal data we collect and why we collect it</h3>
-										
-										<h4 class="title is-5">Comments</h4>
-										<p>When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor's IP address and browser user agent string to help spam detection.</p>
-										<p>An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here: <a href="https://automattic.com/privacy/" target="_blank" class="has-text-link">https://automattic.com/privacy/</a>.</p>
-										<p>After approval of your comment, your profile picture is visible to the public in the context of your comment.</p>
-									</div>
-
-									<div class="box">
-										<h3 class="title is-4">Media</h3>
-										<p>If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included. Visitors to the website can download and extract any location data from images on the website.</p>
-									</div>
-
-									<div class="box">
-										<h3 class="title is-4">Contact forms</h3>
-										
-										<h4 class="title is-5">Cookies</h4>
-										<p>If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment. These cookies will last for one year.</p>
-										<p>If you have an account and you log in to this site, we will set a temporary cookie to determine if your browser accepts cookies. This cookie contains no personal data and is discarded when you close your browser.</p>
-										<p>When you log in, we will also set up several cookies to save your login information and your screen display choices. Login cookies last for two days, and screen options cookies last for a year. If you select "Remember Me", your login will persist for two weeks. If you log out of your account, the login cookies will be removed.</p>
-										<p>If you edit or publish an article, an additional cookie will be saved in your browser. This cookie includes no personal data and simply indicates the post ID of the article you just edited. It expires after 1 day.</p>
-									</div>
-
-									<div class="box">
-										<h3 class="title is-4">Embedded content from other websites</h3>
-										<p>Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.</p>
-										<p>These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p>
-									</div>
-
-									<div class="box">
-										<h3 class="title is-4">How long we retain your data</h3>
-										<p>If you leave a comment, the comment and its metadata are retained indefinitely. This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.</p>
-										<p>For users that register on our website (if any), we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information.</p>
-									</div>
-
-									<div class="box">
-										<h3 class="title is-4">What rights you have over your data</h3>
-										<p>If you have an account on any of our sites, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.</p>
-									</div>
-								</div>
-							</div>
+		<section id="pp" class="section">
+			<div class="container">
+				<div class="has-text-centered mb-6">
+					<h2 class="title is-2">Privacy Policy</h2>
+				</div>
+				<div class="content">
+					<div class="box">
+						<h3 class="title is-4">Who we are</h3>
+						<p>Our website addresses are:</p>
+						<ul>
+							<li>lochstudios.com</li>
+							<li>lochstudios.info</li>
+							<li>lochs.media</li>
+							<li>getyourwebsite.net.au</li>
+							<li>lscomputer.repair</li>
+							<li>sentral.network</li>
+							<li>mystorage-dashboard.com</li>
+							<li>yourhost.zone</li>
+							<li>yourminecraft.host</li>
+						</ul>
+					</div>
+					<div class="box">
+						<h3 class="title is-4">What personal data we collect and why we collect it</h3>
+						
+						<h4 class="title is-5">Comments</h4>
+						<p>When visitors leave comments on the site we collect the data shown in the comments form, and also the visitor's IP address and browser user agent string to help spam detection.</p>
+						<p>An anonymized string created from your email address (also called a hash) may be provided to the Gravatar service to see if you are using it. The Gravatar service privacy policy is available here: <a href="https://automattic.com/privacy/" target="_blank" class="has-text-link">https://automattic.com/privacy/</a>.</p>
+						<p>After approval of your comment, your profile picture is visible to the public in the context of your comment.</p>
+					</div>
+					<div class="box">
+						<h3 class="title is-4">Media</h3>
+						<p>If you upload images to the website, you should avoid uploading images with embedded location data (EXIF GPS) included. Visitors to the website can download and extract any location data from images on the website.</p>
+					</div>
+					<div class="box">
+						<h3 class="title is-4">Contact forms</h3>
+						
+						<h4 class="title is-5">Cookies</h4>
+						<p>If you leave a comment on our site you may opt-in to saving your name, email address and website in cookies. These are for your convenience so that you do not have to fill in your details again when you leave another comment. These cookies will last for one year.</p>
+						<p>If you have an account and you log in to this site, we will set a temporary cookie to determine if your browser accepts cookies. This cookie contains no personal data and is discarded when you close your browser.</p>
+						<p>When you log in, we will also set up several cookies to save your login information and your screen display choices. Login cookies last for two days, and screen options cookies last for a year. If you select "Remember Me", your login will persist for two weeks. If you log out of your account, the login cookies will be removed.</p>
+						<p>If you edit or publish an article, an additional cookie will be saved in your browser. This cookie includes no personal data and simply indicates the post ID of the article you just edited. It expires after 1 day.</p>
+					</div>
+					<div class="box">
+						<h3 class="title is-4">Embedded content from other websites</h3>
+						<p>Articles on this site may include embedded content (e.g. videos, images, articles, etc.). Embedded content from other websites behaves in the exact same way as if the visitor has visited the other website.</p>
+						<p>These websites may collect data about you, use cookies, embed additional third-party tracking, and monitor your interaction with that embedded content, including tracking your interaction with the embedded content if you have an account and are logged in to that website.</p>
+					</div>
+					<div class="box">
+						<h3 class="title is-4">How long we retain your data</h3>
+						<p>If you leave a comment, the comment and its metadata are retained indefinitely. This is so we can recognize and approve any follow-up comments automatically instead of holding them in a moderation queue.</p>
+						<p>For users that register on our website (if any), we also store the personal information they provide in their user profile. All users can see, edit, or delete their personal information at any time (except they cannot change their username). Website administrators can also see and edit that information.</p>
+					</div>
+					<div class="box">
+						<h3 class="title is-4">What rights you have over your data</h3>
+						<p>If you have an account on any of our sites, or have left comments, you can request to receive an exported file of the personal data we hold about you, including any data you have provided to us. You can also request that we erase any personal data we hold about you. This does not include any data we are obliged to keep for administrative, legal, or security purposes.</p>
+					</div>
+				</div>
+			</div>
+		</section>
 		<!-- Footer -->
 		<footer class="footer has-background-dark has-text-white">
 			<div class="container">
